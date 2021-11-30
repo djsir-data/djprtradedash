db3b5ccb579e3814108f3371e242d06d23469b12
Matt Cowgill <mattcowgill@gmail.com>
2021-11-30 17:08:20 +1100

    refine data explorer

--- R/viz_merch.R
+++ R/viz_merch.R
@@ -31,14 +31,6 @@ viz_merch_explorer <- function(data = merch,
       dplyr::mutate(value = slider::slide_mean(.data$value, before = 11L))
   }
 
-  # df %>%
-  #   djpr_ts_linechart(
-  #     col_var = .data$col,
-  #     group_var = .data$group,
-  #     label_single_line = TRUE
-  #   ) +
-  #   facet_wrap(facets = facet_by)
-
   n_col <- length(unique(df$col))
   cols <- if (n_col <= 10) {
       suppressWarnings(djprtheme::djpr_pal(n_col))
@@ -47,14 +39,57 @@ viz_merch_explorer <- function(data = merch,
 
     }
 
-  df %>%
+  total_col_chars <- df$col %>%
+    unique() %>%
+    paste(collapse = " ") %>%
+    nchar()
+
+  show_legend <- if_else(total_col_chars >= 100,
+                         TRUE, FALSE)
+
+  date_limits <- c(min(data$date), max(data$date))
+  x_breaks <- djprtheme::breaks_right(limits = date_limits,
+                                      n_breaks = 5)
+
+  p <- df %>%
     ggplot(aes(x = .data$date,
                y = .data$value,
                col = .data$col,
                group = .data$group)) +
     geom_line() +
+    geom_point(data = ~dplyr::group_by(., .data$group) %>%
+                 dplyr::filter(.data$date == max(.data$date)),
+               fill = "white",
+               show.legend = FALSE,
+               stroke = 1.5, size = 2.5, shape = 21) +
     scale_colour_manual(values = cols) +
-    facet_wrap(facets = facet_by) +
-    theme_djpr()
+    facet_wrap(facets = facet_by)
 
+  if (show_legend) {
+    p <- p +
+      theme_djpr(legend = "top") +
+      theme(legend.direction = "vertical",
+            legend.text = element_text(size = 11)) +
+      scale_x_date(breaks = x_breaks,
+                   date_labels = "%b\n%Y")
+  } else {
+    days_in_data <- as.numeric(max(data$date) - min(data$date))
+    p <- p +
+      ggrepel::geom_text_repel(data = ~filter(.,
+                                     .data$date == max(.data$date)),
+                      aes(label = stringr::str_wrap(.data$col,
+                                                    10)),
+                      size = 11 / .pt,
+                      lineheight = 0.9,
+                      hjust = 0,
+                      nudge_x = days_in_data * 0.033,
+                      seed = 123, show.legend = FALSE,
+                      direction = "y") +
+      scale_x_date(expand = expansion(mult = c(0, 0.28)),
+                   breaks = x_breaks, date_labels = "%b\n%Y") +
+      theme_djpr()
+
+  }
+
+  p
 }
