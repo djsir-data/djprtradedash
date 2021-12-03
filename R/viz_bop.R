9fa4bd85a3d7ae79c7895662eb4efffc70ce4c64
Emma Seyoum-Tegegn (DJPR) <emma.seyoum-tegegn@ecodev.vic.gov.au>
2021-12-03 16:10:48 +1100

    Create bar chart function

--- R/viz_bop.R
+++ R/viz_bop.R
@@ -119,4 +119,80 @@ viz_services_bop_line_chart <- function(data = bop)
     )
 }
 
+viz_good_bop_bar_chart <- function(data = bop)
+{
+
+
+
+
+  df <- data %>%
+    dplyr::select(-.data$series_id, -.data$unit) %>%
+    dplyr::filter(.data$goods_services == "Services", .data$indicator == "Chain Volume Measures") %>%
+    dplyr::mutate(value = abs(.data$value)) %>%
+    dplyr::mutate(state = dplyr::case_when(.data$state == "Australian Capital Territory",
+                                       "ACT",
+                                       .data$state
+  ))
+
+  df <- df %>%
+    dplyr::group_by(.data$state, .data$exports_imports) %>%
+    dplyr::mutate(value = 100 * ((.data$value / .data$value[date == as.Date("2019-12-01")]) - 1)) %>%
+    dplyr::ungroup()
+
+  df <- df %>%
+    dplyr::group_by(.data$state) %>%
+    dplyr::filter(.data$date == max(.data$date)) %>%
+    dplyr::ungroup()
+
+
+  lab_df <- data %>%
+    dplyr::select(.data$state, .data$value) %>%
+    dplyr::mutate(
+      lab_y = dplyr::if_else(.data$value >= 0, .data$value + 0.1, .data$value - 0.75),
+      lab_hjust = dplyr::if_else(.data$value >= 0, 0, 1)
+    )
+
+
+
+  # draw bar chart for all state
+  df %>%
+    ggplot(aes(
+      x = stats::reorder(.data$state, .data$value),
+      y = .data$value
+    )) +
+    geom_col(
+      aes(fill = -.data$value)
+    ) +
+    geom_text(
+      data = lab_df,
+      aes(
+        y = .data$lab_y,
+        hjust = .data$lab_hjust,
+        label = paste0(round2(.data$value, 1), "%")
+      ),
+      colour = "black",
+      size = 11 / .pt
+    ) +
+    geom_hline(
+      yintercept = 0
+    ) +
+    coord_flip(clip = "off") +
+    scale_y_continuous(expand = expansion(mult = c(0.2, 0.15))) +
+    scale_fill_distiller(palette = "Blues") +
+    djprtheme::theme_djpr(flipped = TRUE) +
+    theme(
+      axis.title.x = element_blank(),
+      panel.grid = element_blank(),
+      axis.text.y = element_text(size = 12),
+      axis.text.x = element_blank()
+    ) +
+    labs(
+      title = "title",
+      subtitle = paste0(
+        "Growth in export and import of services between December 2019 and ",
+        format(max(data$date), "%B %Y")
+      ),
+      paste0("Seasonally Adjusted Chain Volume Measures")
+    )
+}
 
