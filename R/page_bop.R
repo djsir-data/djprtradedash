b6ffda93d457d14ce728314c3f4abd529ae80eff
Emma Seyoum-Tegegn (DJPR) <emma.seyoum-tegegn@ecodev.vic.gov.au>
2021-12-07 13:38:34 +1100

    Page_bop, ui page created

---
+++ R/page_bop.R
@@ -0,0 +1,13 @@
+page_bop <- function(...) {
+  djpr_tab_panel(
+    title = "Balance of Payments",
+    h1("Key indicators"),
+    paste0("This page contains Victoria's international transactions, typically quarterly or
+    yearly, over a particular period.  It shows the sum of the transactions of those involving
+    goods or services."),
+
+    h2(br(), "Goods"),
+    h2(br(), "Sevices"),
+    h2(br(), "Balance of Trade "),
+  )
+}
