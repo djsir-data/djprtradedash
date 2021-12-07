4e99bbd06de69e7f14955ace20c87c010f11ad06
Emma Seyoum-Tegegn (DJPR) <emma.seyoum-tegegn@ecodev.vic.gov.au>
2021-12-07 13:47:34 +1100

    page_bop() is added to the server side

--- R/ui.R
+++ R/ui.R
@@ -3,6 +3,7 @@ ui <- function() {
   djprshiny::djpr_page(
     title = shiny::HTML("DJPR Trade<br>Dashboard"),
     page_launchpad(),
-    page_merch()
-  )
+    page_merch(),
+    page_bop()
+    )
 }
