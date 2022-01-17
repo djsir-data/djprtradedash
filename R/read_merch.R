d28c067104b36b53557deb028cb9e1f7cc015e61
Matt De Pasquale <mattjdepasquale@gmail.com>
2022-01-17 15:06:12 +1100

    Alter path to account for changes to ABS file naming convention

--- R/read_merch.R
+++ R/read_merch.R
@@ -29,9 +29,10 @@ read_merch <- function(path = tempdir(),
     url <- "https://www.abs.gov.au/websitedbs/D3110132.nsf/home/DataExplorer/$File/MERCH_EXP.zip"
     exports_dest_zip <- file.path(path, basename(url))
     download.file(url, exports_dest_zip, mode="wb")
-    unzip(exports_dest_zip, exdir=path, unzip="unzip")
+    utils::unzip(exports_dest_zip, exdir=path)
+    exp_csv<- list.files(path, pattern="^MERCH_EXP_",full.names=TRUE)
     merch <- data.table::fread(
-      file.path(path, "MERCH_EXP.csv"),
+      file.path(exp_csv),
       stringsAsFactors = TRUE,
       data.table = TRUE
       )
@@ -90,9 +91,10 @@ read_merch <- function(path = tempdir(),
     url <- "https://www.abs.gov.au/websitedbs/D3110132.nsf/home/DataExplorer/$File/MERCH_IMP.zip"
     imports_dest_zip <- file.path(path, basename(url))
     download.file(url, imports_dest_zip, mode="wb")
-    unzip(imports_dest_zip, exdir=path, unzip="unzip")
+    utils::unzip(imports_dest_zip, exdir=path)
+    imp_csv<- list.files(path, pattern="^MERCH_IMP_",full.names=TRUE)
     merch <- data.table::fread(
-      file.path(path, "MERCH_IMP.csv"),
+      file.path(imp_csv),
       stringsAsFactors = TRUE,
       data.table = TRUE
       )
