948e24ab2a572fa8409727a3a6597b711b17a02c
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-01-28 15:17:21 +1100

    Updating country profiles

--- R/page_country_profile.R
+++ R/page_country_profile.R
@@ -6,9 +6,38 @@ page_country_profile <- function(...) {
     br(),
     centred_row(
       tagList(
+        br(),
         selectInput("country_select", "Country", ""),
+        br(),
         h1(textOutput("country_select")),
-        p("to be completed"),
+        br(),
+        fluidRow(
+          column(
+            4,
+            p("99%", style = "font-size: 400%;text-align: center;font-weight: bold;"),
+            p(
+              "Year on year export change",
+              style = "text-align: center;font-style: italic;"
+              )
+            ),
+          column(
+            4,
+            p("99%", style = "font-size: 400%;text-align: center;font-weight: bold;"),
+            p(
+              "Year on year export change",
+              style = "text-align: center;font-style: italic;"
+            )
+          ),
+          column(
+            4,
+            p("99%", style = "font-size: 400%;text-align: center;font-weight: bold;"),
+            p(
+              "Year on year export change",
+              style = "text-align: center;font-style: italic;"
+            )
+          )
+        ),
+        br(),
         djpr_plot_ui("country_top_exp")
       )
     )
