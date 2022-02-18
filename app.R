a8187a695ac18e1622b733b152c645dc74bf7c23
Matt Johnson <mrjoh3@gmail.com>
2022-02-18 16:53:10 +1100

    switch to dbplyr

--- app.R
+++ app.R
@@ -1,2 +1,30 @@
 pkgload::load_all(".")
+
+# testing overwrite merch with db connection
+
+
+# setup db connection
+library(pool)
+library(dplyr)
+library(dbplyr)
+
+db_pool <- dbPool(
+  drv = duckdb::duckdb(),
+  db = "trade"
+)
+
+
+
+merch <- tbl(db_pool, 'merch') # collect all here is 6 sec
+merch_imp <- tbl(db_pool, 'merch_imp')
+supp_cy <- tbl(db_pool, 'supp_cy')
+supp_fy <- tbl(db_pool, 'supp_fy')
+bop <- tbl(db_pool, 'bop')
+
 app()
+
+
+
+
+
+
