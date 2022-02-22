4aa0c0fa9302eca095141dedd7d4997285c75ee0
Emma Seyoum-Tegegn (DJPR) <emma.seyoum-tegegn@ecodev.vic.gov.au>
2022-02-22 16:57:10 +1100

    table_export_import added to the server and UI

--- R/page_bop.R
+++ R/page_bop.R
@@ -6,15 +6,22 @@ page_bop <- function(...) {
     yearly, over a particular period.  It shows the sum of the transactions of those involving
     goods or services."),
     h2(br(), "Goods and Services"),
-    djpr_plot_ui("good_services_import_chart"),
+    uiOutput("table_export_import") %>%
+      djpr_with_spinner(),
+    br(),
+    djpr_plot_ui("good_services_export_chart"),
     br(),
     djpr_plot_ui("total_bop_bar_chart",
       interactive = FALSE
     ),
     br(),
+    djpr_plot_ui("good_services_import_chart"),
+    br(),
     djpr_plot_ui("goods_export_import_line"),
     br(),
-    djpr_plot_ui("vic_total_bop_cumul_line"),
+    djpr_plot_ui("Vic_total_bop_bar_chart",
+      interactive = FALSE
+    ),
     h2(br(), "Goods"),
     djpr_plot_ui("goods_bop_bar_chart",
       interactive = FALSE
@@ -33,6 +40,9 @@ page_bop <- function(...) {
     djpr_plot_ui("NSW_Vic_Services_line_chart"),
     br(),
     h2(br(), "Balance of Trade "),
-    djpr_plot_ui("trade_balance_line_chart")
+    djpr_plot_ui("trade_balance_line_chart"),
+    br(),
+    centred_row(htmlOutput("bop_footnote")),
+    br()
   )
 }
