b5bfbc1b53198d3fcd42bc202d30a0887eaa6d6d
Louis-DJPR <louis.cui@ecodev.vic.gov.au>
2022-02-23 14:43:36 +1100

    changing order of pages

--- R/ui.R
+++ R/ui.R
@@ -3,8 +3,8 @@ ui <- function() {
   djprshiny::djpr_page(
     title = shiny::HTML("DJPR Trade<br>Dashboard"),
     page_launchpad(),
-    page_merch(),
     page_bop(),
+    page_merch(),
     page_country_profile()
   )
 }
