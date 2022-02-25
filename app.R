7c3a386f4547d79987533e09a7d0f2fe9fc613b3
Matt Johnson <mrjoh3@gmail.com>
2022-02-25 16:31:25 +1100

    add lookups to global redo db connection to drop pool

--- app.R
+++ app.R
@@ -1,26 +1,38 @@
 pkgload::load_all(".")
 
-# testing overwrite merch with db connection
-
-
 # setup db connection
 library(pool)
 library(dplyr)
 library(dbplyr)
 library(glue)
 
-db_pool <- dbPool(
-  drv = duckdb::duckdb(),
-  db = "trade"
-)
 
+con <- duckdb::dbConnect(drv = duckdb::duckdb(),
+                         db = "trade")
 
+merch <- tbl(con, 'merch') # collect all here is 6 sec
+merch_imp <- tbl(con, 'merch_imp')
+supp_cy <- tbl(con, 'supp_cy')
+supp_fy <- tbl(con, 'supp_fy')
+bop <- tbl(con, 'bop')
 
-merch <- tbl(db_pool, 'merch') # collect all here is 6 sec
-merch_imp <- tbl(db_pool, 'merch_imp')
-supp_cy <- tbl(db_pool, 'supp_cy')
-supp_fy <- tbl(db_pool, 'supp_fy')
-bop <- tbl(db_pool, 'bop')
+merch_dates <- merch |>
+  summarise(
+    min = min(date, na.rm = TRUE),
+    max = max(date, na.rm = TRUE)
+    ) |>
+  collect() |>
+  mutate(across(everything(), as.Date))
+
+merch_sitc <- merch |>
+  summarize(sitc = distinct(sitc)) |>
+  collect() |> pull()
+
+merch_country_dest <- merch |>
+  summarize(sitc = distinct(country_dest)) |>
+  collect() |> pull()
+
+in_global <- ls()
 
 app()
 
