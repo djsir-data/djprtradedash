072524d3af615be7b2cdf653c3db8013f2061e33
Matt Johnson <mrjoh3@gmail.com>
2022-02-25 16:34:22 +1100

    re enable other pages

--- R/ui.R
+++ R/ui.R
@@ -2,9 +2,9 @@
 ui <- function() {
   djprshiny::djpr_page(
     title = shiny::HTML("DJPR Trade<br>Dashboard"),
-    page_launchpadUI('lp') #,
-    #page_merch(),
-    #page_bop(),
+    page_launchpadUI('lp'),
+    page_merch(),
+    page_bopUI()#,
     #page_country_profile()
   )
 }
