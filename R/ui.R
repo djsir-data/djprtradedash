516539833eee5fc48367ac1619fef57cab91b262
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-03-04 16:31:35 +1100

    Remove country profile tab (temporary)

--- R/ui.R
+++ R/ui.R
@@ -4,7 +4,7 @@ ui <- function() {
     title = shiny::HTML("DJPR Trade<br>Dashboard"),
     page_launchpad(),
     page_bop(),
-    page_merch(),
-    page_country_profile()
+    page_merch()#,
+    # page_country_profile()
   )
 }
