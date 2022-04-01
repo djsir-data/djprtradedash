ec1930eebc942bbfdc00cea4c066f111955888cc
Matt Johnson <mrjoh3@gmail.com>
2022-04-01 14:57:27 +1100

    add vic font to app, text still needs to be styled

--- R/ui.R
+++ R/ui.R
@@ -1,6 +1,26 @@
 
 ui <- function() {
-  djprshiny::djpr_page(
+  djprshiny::djpr_page(col_widths = c(1,10,1),
+                       tags$style(HTML("@font-face {
+                                          font-family: vic-bold;
+                                          src: url(https://raw.githubusercontent.com/djpr-data/blank_rmd_project/main/fonts/VIC-Bold.woff2);
+                                        }")),
+                       tags$style(HTML("@font-face {
+                                          font-family: vic-semibold;
+                                          src: url(https://raw.githubusercontent.com/djpr-data/blank_rmd_project/main/fonts/VIC-SemiBold.woff2);
+                                        }")),
+                       tags$style(HTML("@font-face {
+                                          font-family: vic-medium;
+                                          src: url(https://raw.githubusercontent.com/djpr-data/blank_rmd_project/main/fonts/VIC-Medium.woff2);
+                                        }")),
+                       tags$style(HTML("@font-face {
+                                          font-family: vic-regular;
+                                          src: url(https://raw.githubusercontent.com/djpr-data/blank_rmd_project/main/fonts/VIC-Regular.woff2);
+                                        }")),
+                       tags$style(HTML("@font-face {
+                                          font-family: vic-light;
+                                          src: url(https://raw.githubusercontent.com/djpr-data/blank_rmd_project/main/fonts/VIC-Light.woff2);
+                                        }")),
     title = shiny::HTML("DJPR Trade<br>Dashboard"),
     page_launchpadUI('lp'),
     page_merchUI(),
