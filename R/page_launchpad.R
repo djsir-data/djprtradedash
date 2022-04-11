bb764e42591418697f51c5e76e45f304b920c877
Matt Johnson <mrjoh3@gmail.com>
2022-04-11 18:01:05 +1000

    incorporate launchpad nav cards

--- R/page_launchpad.R
+++ R/page_launchpad.R
@@ -1,45 +1,130 @@
 page_launchpadUI <- function(id) {
 
-  shiny::fluidRow(
-    djprshiny::djpr_h2_box("Overview"),
-    djprshiny::djpr_plot_box(
-      id = "top_export_line_chart",
-      interactive = TRUE,
-      width = 12
+  shiny::tagList(
+      shiny::fluidRow(
+        djprshiny::djpr_h2_box("DJPR Trade Dashboard")),
+      shiny::fluidRow(
+        column(5,
+               shinyWidgets::panel(
+                 style = 'height:530px;',
+                 'add some text here')),
+        column(7,
+               djprshiny::djpr_plot_box(
+                 id = "top_export_line_chart",
+                 interactive = TRUE,
+                 width = 12
+               ))
       ),
-    djprshiny::djpr_plot_box(
-      "good_services_export_line_launchpad",
-      interactive = TRUE,
-    ),
-    djprshiny::djpr_plot_box(
-      "top_country_line_chart",
-      interactive = TRUE,
-    ),
-    shiny::br(),
-    djprshiny::djpr_h2_box("Countries"),
-    shinydashboard::box(
-      title = h3("Top 5 Exports ($m)"),
-      shiny::uiOutput("country_export_table", height = "600px")
-    ),
-    shinydashboard::box(
-      title = h3("Top 5 Imports ($m)"),
-      shiny::uiOutput("country_import_table", height = "600px")
-    ),
-    djprshiny::djpr_h2_box("Products"),
-    shinydashboard::box(
-      title = h3("Top 5 Exports ($m)"),
-      shiny::uiOutput("product_export_table", height = "600px")
-    ),
-    shinydashboard::box(
-      title = h3("Top 5 Imports ($m)"),
-      shiny::uiOutput("product_import_table", height = "600px")
-    ),
-    djprshiny::djpr_h2_box("Balance of payments"),
-    shinydashboard::box(
-      shiny::uiOutput("launchpad_bop_table", height = "600px"),
-      width = 12
+      shiny::fluidRow(style = 'padding:20px;',
+        shiny::column(4,
+
+                      div(class = 'card text-white bg-dark mb-3',
+                          style = 'height:200px;',
+                          div(class = 'card-header',
+                              h4('Exports Explorer')
+                          ),
+                          div(class = 'card-body',
+                              div(class = 'row',
+                                  div(class = 'col',
+                                      'Description of what to find in merch explorer',
+                                      div(style = 'position:absolute;bottom:20px',
+                                          shinyWidgets::actionBttn('action1', "Explore",
+                                                                   style = 'material-flat',
+                                                                   color = 'success'))),
+                                  div(class = 'col-6',
+                                      style = "text-align: right;padding-bottom:15px;",
+                                      img(style = 'display:inline;height:10vh;width:auto;',
+                                          src = 'https://icongr.am/material/binoculars.svg?size=164&color=fafafa',
+                                          alt = 'small icon of binoculars')))
+                          )
+                      )
+      ),
+      shiny::column(4,
+
+                    div(class = 'card text-white bg-dark mb-3',
+                        style = 'height:200px;',
+                        div(class = 'card-header',
+                            h4("Balance of Payments")),
+                        div(class = 'card-body',
+                            div(class = 'row',
+                                div(class = 'col',
+                                    'description of balance of payments',
+                                    div(style = 'position:absolute;bottom:20px',
+                                        shinyWidgets::actionBttn('action2', "Balance",
+                                                                 style = 'material-flat',
+                                                                 color = 'success'))),
+                                div(class = 'col-6',
+                                    style = "text-align: right;padding-bottom:15px;",
+                                    img(style = 'display:inline;height:10vh;width:auto;',
+                                        src = 'https://icongr.am/clarity/balance.svg?size=164&color=fafafa',
+                                        alt = 'small icon of weighing balance')))
+                        )
+                    )
+      ),
+      shiny::column(4,
+
+                    div(class = 'card text-white bg-dark mb-3',
+                        style = 'height:200px;',
+                        div(class = 'card-header',
+                            h4("Help and Methodology")),
+                        div(class = 'card-body',
+                            div(class = 'row',
+                                div(class = 'col',
+                                    'some text here',
+                                    div(style = 'position:absolute;bottom:20px',
+                                        shinyWidgets::actionBttn('action3', 'Help',
+                                                                 style = 'material-flat',
+                                                                 color = 'success'))),
+                                div(class="col-6",
+                                    style = "text-align: right;padding-bottom:15px;",
+                                    img(style = 'display:inline;height:10vh;width:auto;',
+                                        src = 'https://icongr.am/clarity/help-info.svg?color=fafafa',
+                                        alt = 'small icon of I for')))
+                        )
+                    )
+      )),
+    fluidRow(
+
+      djprshiny::djpr_plot_box(
+        "good_services_export_line_launchpad",
+        interactive = TRUE,
+      ),
+      djprshiny::djpr_plot_box(
+        "top_country_line_chart",
+        interactive = TRUE,
+      ),
+      shiny::br(),
+      djprshiny::djpr_h2_box("Countries"),
+      shinydashboard::box(
+        title = h3("Top 5 Exports ($m)"),
+        shiny::uiOutput("country_export_table", height = "600px")
+      ),
+      shinydashboard::box(
+        title = h3("Top 5 Imports ($m)"),
+        shiny::uiOutput("country_import_table", height = "600px")
+      ),
+      djprshiny::djpr_h2_box("Products"),
+      shinydashboard::box(
+        title = h3("Top 5 Exports ($m)"),
+        shiny::uiOutput("product_export_table", height = "600px")
+      ),
+      shinydashboard::box(
+        title = h3("Top 5 Imports ($m)"),
+        shiny::uiOutput("product_import_table", height = "600px")
+      ),
+      djprshiny::djpr_h2_box("Balance of payments"),
+      shinydashboard::box(
+        shiny::uiOutput("launchpad_bop_table", height = "600px"),
+        width = 12
+      )
+
     )
+
   )
+
+
+
+
 
 }
 
