a6b2ef83e537a019ce391cf72811b43bf18a1f48
Henry S McMillan (DJPR) <henry.mcmillan@ecodev.vic.gov.au>
2022-05-06 17:48:49 +1000

    start bop tidy

--- R/page_bop.R
+++ R/page_bop.R
@@ -1,49 +1,44 @@
 page_bopUI <- function(...) {
   shiny::fluidRow(
-    # title = "Balance of Payments",
-    shiny::h1(
-      shiny::span("Balance of payments",
-           style = "font-size: 40px; color: #1F1547"
-      )
-    ),
-    shiny::p(
-      "Summary information on Victoria's quarterly trade",
-      style = "font-size: 150%;font-style: italic;color: #A9A9A9"
-      ),
-    shiny::br(),
-    shiny::br(),
-    shiny::h2("Goods and Services", align = "center"),
-    shiny::br(),
+
+    # Goods and services
+    djprshiny::djpr_h2_box("Goods and services"),
     djprshiny::djpr_plot_ui("good_services_chart"),
-    shiny::br(),
+    s
     djprshiny::djpr_plot_ui("total_bop_bar_chart",
       interactive = FALSE
     ),
-    shiny::br(),
+
     djprshiny::djpr_plot_ui("goods_export_import_line"),
-    shiny::br(),
+
+
+    # Goods
     shiny::h2("Goods", align = "center"),
     djprshiny::djpr_plot_ui("goods_bop_bar_chart",
       interactive = FALSE
     ),
-    shiny::br(),
+
     djprshiny::djpr_plot_ui("good_trade_line_chart"),
-    shiny::br(),
+
     djprshiny::djpr_plot_ui("NSW_Vic_goods_line_chart"),
-    shiny::br(),
-    shiny::br(),
+
+
+
+    # Services
     shiny::h2("Services", align = "center"),
-    shiny::br(),
+
     djprshiny::djpr_plot_ui("service_bop_bar_chart",
       interactive = FALSE
     ),
-    shiny::br(),
+
     djprshiny::djpr_plot_ui("services_trade_line_chart"),
     djprshiny::djpr_plot_ui("NSW_Vic_Services_line_chart"),
-    shiny::br(),
-    shiny::br(),
+
+
+
+    # Balance of trade
     shiny::h2("Balance of Trade", align = "center"),
-    shiny::br(),
+
     djprshiny::djpr_plot_ui("trade_balance_line_chart")
   )
 }
