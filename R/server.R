ab97159572b648c7867341d2bd75ee5f13ace230
Henry S McMillan (DJPR) <henry.mcmillan@ecodev.vic.gov.au>
2022-05-12 16:39:02 +1000

    server cleanup

--- R/server.R
+++ R/server.R
@@ -1,40 +1,6 @@
 
 server <- function(input, output, session) {
 
-  plt_change <- shiny::reactive(input$plt_change) %>%
-    shiny::debounce(2)
-
-  #Launchpad searchbar
-  server <- function(input, output, session) {
-    output$res <- shiny::renderPrint({
-      input$search
-    })
-  }
-
-  # Page Footnotes
-
-  footnote <- shiny::reactive({
-    # req(dash_data)
-    # latest <- max(series_latestdates)
-    shiny::div(
-      shiny::HTML(
-        paste0(
-          "This dashboard is produced by the <b>Strategy and Priority ",
-          "Projects - Data + Analytics</b> team at the Victorian Department ",
-          "of Jobs, Precincts and Regions. ", # The <b>latest data in this ",
-          # "dashboard is for ",
-          # format(latest, "%B %Y"),
-          'Please <a href="mailto:spp-data@ecodev.vic.gov.au?subject=DJPR Trade Dashboard">email us</a> with any comments or feedback.'
-        )
-      ),
-      style = "color: #828282; font-size: 0.75rem"
-    )
-  })
-
-  output$methodology_footnote <- output$launchpad_footnote <- output$bop_footnote <- output$merch_footnote <- output$country_footnote <- shiny::renderUI({
-    footnote()
-  })
-
   page_launchpad(input, output, session, table_rowcount = 5)
   page_bop(input, output, session, plt_change, table_rowcount = 5)
   page_merch(input, output, session, plt_change)
