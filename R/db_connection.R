54e60810706ae30623459d051a9ddec4241ffd99
Henry S McMillan (DJPR) <henry.mcmillan@ecodev.vic.gov.au>
2022-08-12 09:11:04 +1000

    add cached DB option and minor refactoring

---
+++ R/db_connection.R
@@ -0,0 +1,98 @@
+# Assign multiple lazy data frames to global environement
+assign_table_global <- function(con, tables){
+  lapply(tables, function(tab){
+    assign(tab, dplyr::tbl(con, tab), envir = .GlobalEnv)
+  })
+}
+
+
+
+# load lazy tables for testing
+load_tabs <- function(){
+
+  # Get environment-spesific connection information
+  creds <- config::get("dataconnection")
+
+  # If running in chached database mode, load data
+  if(creds$use_DBcache == TRUE){
+    load("DBcache.rData")
+    return(NULL)
+  }
+
+  # Do not connect if there is already a working connection
+  if(exists("con", envir = .GlobalEnv)){
+    if(pool::dbIsValid(con)){
+      return(NULL)
+    }
+  }
+
+  assign(
+    x = "con",
+    value = pool::dbPool(
+      drv = RPostgres::Postgres(),
+      dbname = creds$dbname,
+      host = creds$host,
+      user = creds$user,
+      password = creds$password,
+      port = creds$port
+    ),
+    envir = .GlobalEnv
+  )
+
+  assign_table_global(
+    con = con,
+    tables = c(
+      "merch",
+      "merch_imp",
+      "service_trade",
+      "bop"
+    )
+  )
+
+  merch %>%
+    dplyr::summarise(
+      min = min(date, na.rm = TRUE),
+      max = max(date, na.rm = TRUE)
+    ) %>%
+    dplyr::collect()  %>%
+    dplyr::mutate(dplyr::across(dplyr::everything(), as.Date)) %>%
+    assign("merch_dates", ., envir = .GlobalEnv)
+
+  bop %>%
+    dplyr::summarise(
+      min = min(date, na.rm = TRUE),
+      max = max(date, na.rm = TRUE)
+    ) %>%
+    dplyr::collect()  %>%
+    dplyr::mutate(dplyr::across(dplyr::everything(), as.Date)) %>%
+    assign("bop_dates", ., envir = .GlobalEnv)
+
+  merch %>%
+    dplyr::group_by(sitc, sitc_code) %>%
+    dplyr::summarize(n = length(sitc_code)) %>%
+    dplyr::collect() %>%
+    assign("merch_sitc_lu", ., envir = .GlobalEnv)
+
+  merch %>%
+    dplyr::summarize(sitc = dplyr::distinct(country_dest))  %>%
+    dplyr::collect() %>%
+    dplyr::pull() %>%
+    assign("merch_country_dest", ., envir = .GlobalEnv)
+
+  service_trade %>%
+    dplyr::summarise(
+      min = min(date, na.rm = TRUE),
+      max = max(date, na.rm = TRUE)
+    ) %>%
+    dplyr::collect()  %>%
+    dplyr::mutate(dplyr::across(dplyr::everything(), as.Date)) %>%
+    assign("service_dates", ., envir = .GlobalEnv)
+
+  service_trade %>%
+    arrange(desc(value)) %>%
+    select(service) %>%
+    collect() %>%
+    pull() %>%
+    unique() %>%
+    assign("service_categories", ., envir = .GlobalEnv)
+}
--- app.R
+++ app.R
@@ -6,6 +6,9 @@ pkgload::load_all(".")
 # Load environment variables (if custom file exists)
 if(file.exists(".renviron")) readRenviron(".renviron")
 
+# Optional cached DB mode (use cacheDB() to create)
+Sys.setenv(R_CONFIG_ACTIVE = "local_cache")
+
 # Establish database connections
 load_tabs()
 
