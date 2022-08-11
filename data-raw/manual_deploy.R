12a280bbf67c79e08efbe26b162e13c9248bab66
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-08-11 20:13:17 +1000

    db credential fix

--- data-raw/manual_deploy.R
+++ data-raw/manual_deploy.R
@@ -2,15 +2,15 @@
 
 # Write DB connection environment variables
 if(
-  !is.null(getOption("PG_READ_OPEN_USER")) &
-  !is.null(getOption("PG_READ_OPEN_PW")) &
+  !is.null(Sys.getenv("PG_READ_OPEN_USER")) &
+  !is.null(Sys.getenv("PG_READ_OPEN_PW")) &
   !file.exists(".renviron")
 ){
   writeLines(
     con = ".renviron",
     text = c(
-      paste("PG_READ_OPEN_USER", "=", getOption("PG_READ_OPEN_USER")),
-      paste("PG_READ_OPEN_PW", "=", getOption("PG_READ_OPEN_PW"))
+      paste("PG_READ_OPEN_USER", "=", Sys.getenv("PG_READ_OPEN_USER")),
+      paste("PG_READ_OPEN_PW", "=", Sys.getenv("PG_READ_OPEN_PW"))
     )
   )
 } else {
