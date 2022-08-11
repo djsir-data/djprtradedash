07507df284f5196ba38380b395083ab2f147e4e3
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-08-11 20:05:26 +1000

    safe db credential handling

--- app.R
+++ app.R
@@ -3,6 +3,9 @@
 # Load objects from this project
 pkgload::load_all(".")
 
+# Load environment variables (if custom file exists)
+if(file.exists(".renviron")) readRenviron(".renviron")
+
 # Establish database connections
 load_tabs()
 
--- data-raw/manual_deploy.R
+++ data-raw/manual_deploy.R
@@ -1,5 +1,28 @@
-options(rsconnect.max.bundle.size = 5e+9)
+
+
+# Write DB connection environment variables
+if(
+  !is.null(getOption("PG_READ_OPEN_USER")) &
+  !is.null(getOption("PG_READ_OPEN_PW")) &
+  !file.exists(".renviron")
+){
+  writeLines(
+    con = ".renviron",
+    text = c(
+      paste("PG_READ_OPEN_USER", "=", getOption("PG_READ_OPEN_USER")),
+      paste("PG_READ_OPEN_PW", "=", getOption("PG_READ_OPEN_PW"))
+    )
+  )
+} else {
+  stop("database credentials not present as environment variable")
+}
+
+# Publish App
 rsconnect::deployApp(
-  appName = "trade-henry",
+  appName = "gv-test",
   forceUpdate = TRUE
 )
+
+
+# Remove renviron file
+unlink(".renviron")
