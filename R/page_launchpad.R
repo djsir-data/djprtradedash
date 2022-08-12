e7ff9fdd8b113f85e61867e2fbbec8ab3dcc7edf
Henry S McMillan (DJPR) <henry.mcmillan@ecodev.vic.gov.au>
2022-08-12 15:35:10 +1000

    launchpad title box

--- R/page_launchpad.R
+++ R/page_launchpad.R
@@ -3,6 +3,59 @@ page_launchpadUI <- function(id) {
 
   shiny::fluidPage(
 
+    shinydashboard::box(
+      title = "Victorian trade data",
+      width = 12,
+      collapsible = T,
+      p(
+        "This dashboard explores the latest Australian Bureau of Statisitics",
+        " (ABS) data on Victoria's trade of goods and services. "
+      ),
+      tags$ul(
+        class = "fa-ul",
+        tags$li(
+          span(class = "fa-li", tags$i(class = "fa fa-caret-left")),
+          a(
+            class = "merchLink",
+            "Find your merchandise export market's performance")
+          ),
+        tags$li(
+          span(class = "fa-li", tags$i(class = "fa fa-caret-left")),
+          a(
+            class = "bopLink",
+            "Explore Victoria's overall trade performance"
+            )
+          ),
+        tags$li(
+          span(class = "fa-li", tags$i(class = "fa fa-caret-left")),
+          a(
+            class = "servicesLink",
+            "Compare Victoria's service exports"
+          )
+        )
+      )
+    ) %>%
+      tagAppendAttributes(
+        style = "background:var(--twilight);background-image: url(\"containers.png\");background-repeat: no-repeat;border-radius: 1.25rem;min-height: 32px;max-height: 340px;background-size: 30% 340px;",
+        .cssSelector = ".box"
+        ) %>%
+      tagAppendAttributes(
+        style = "padding-left:33%;font-size:16px;",
+        .cssSelector = ".box-body"
+      ) %>%
+      tagAppendAttributes(
+        style = "padding-left:33%;",
+        .cssSelector = ".box-header"
+      ) %>%
+      tagAppendAttributes(
+        style = "font-size:20px;font-weight:bold;",
+        .cssSelector = ".box-header h3"
+      ) %>%
+      to_col_xl() %>%
+      fluidRow(),
+
+
+
     # Launchpad text & export plot
     "Victorian trade at a glance" %>%
       h2() %>% div(class = "inner") %>%
