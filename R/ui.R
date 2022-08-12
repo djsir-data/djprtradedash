6ef33c2b26d0a09c14bc10a84e12158243162797
Henry S McMillan (DJPR) <henry.mcmillan@ecodev.vic.gov.au>
2022-08-12 15:35:39 +1000

    Javascript links and chart resize

--- R/ui.R
+++ R/ui.R
@@ -30,16 +30,21 @@ ui <- function() {
     ),
     body = shinydashboard::dashboardBody(
       tags$head(
-        # includeCSS("www/tradedash.css")
-        # shiny::tags$link(rel = "stylesheet", type = "text/css", href = "djprshiny/dashboard.css"),
-        # shiny::tags$link(rel = "stylesheet", type = "text/css", href = "djprshiny/bs5-card2.css"),
+        # Bootstrap 5 for xl columns
         tags$link(
           href = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
           rel = "stylesheet",
           integrity = "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC",
           crossorigin = "anonymous"
         ),
-        includeCSS("www/globalvic.css")
+        # Gloabl vic css
+        tags$link(href = "globalvic.css", rel = "stylesheet"),
+        # Launchpad links (client side execution)
+        shiny::tags$script("$(document).ready(function(){$('a.merchLink').click(function(){$('.sidebar-menu a[data-value=\"merch\"]').trigger('click');})});"),
+        shiny::tags$script("$(document).ready(function(){$('a.bopLink').click(function(){$('.sidebar-menu a[data-value=\"bop\"]').trigger('click');})});"),
+        shiny::tags$script("$(document).ready(function(){$('a.servicesLink').click(function(){$('.sidebar-menu a[data-value=\"services\"]').trigger('click');})});"),
+        # Resize highcharts on sidebar collapse
+        shiny::tags$script(src = "sidebar_chart_resize.JS")
         ),
       shiny::tags$script("$('html').attr(\"lang\", \"en\")"),
       shiny::tags$script("$('section.content').attr(\"role\", \"main\")"),
