aae28a57a0519470b20ab379dd2d1f51ab39c54f
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-08-15 13:24:13 +1000

    service page formatting

--- R/page_services.R
+++ R/page_services.R
@@ -41,46 +41,56 @@ page_servicesUI <- function(...){
       div(class = "small-box") %>% column(12, .) %>%
       fluidRow(),
 
-      column(
-        3,
-        div(
-          class = "box",
-          style = "padding:20px;height:500px;overflow-y:visible;",
-          h3("Chart options"),
-          br(),
-          selectInput(
-            "service_state_comp_inp",
-            "Select service export",
-            choices = service_categories,
-            selected = "Education travel",
-            multiple = FALSE
+    fluidRow(
+      shinydashboard::box(
+        title = "Chart options",
+        width = 3,
+        collapsible = TRUE,
+        selectInput(
+          "service_state_comp_inp",
+          "Select service export",
+          choices = service_categories,
+          selected = "Education travel",
+          multiple = FALSE
+        ),
+        checkboxGroupInput(
+          "service_state_comp_states",
+          "Select states to display",
+          choices = c(
+            "Vic.",
+            "NSW",
+            "Qld",
+            "WA",
+            "ACT",
+            "NT",
+            "SA",
+            "Tas."
           ),
-          checkboxGroupInput(
-            "service_state_comp_states",
-            "Select states to display",
-            choices = c(
-              "Vic.",
-              "NSW",
-              "Qld",
-              "WA",
-              "ACT",
-              "NT",
-              "SA",
-              "Tas."
-            ),
-            selected = c(
-              "Vic.",
-              "NSW",
-              "Qld",
-              "WA",
-              "ACT",
-              "NT",
-              "SA",
-              "Tas."
-            )
+          selected = c(
+            "Vic.",
+            "NSW",
+            "Qld",
+            "WA",
+            "ACT",
+            "NT",
+            "SA",
+            "Tas."
           )
         )
-      ),
+      ) %>%
+        tagAppendAttributes(
+          style = "background:var(--twilight);overflow-y:auto;overflow-x:crop;",
+          .cssSelector = ".box"
+        ) %>%
+        tagAppendAttributes(
+          style = "padding:15px;",
+          .cssSelector = ".box-body"
+        ) %>%
+        tagAppendAttributes(
+          style = "font-size:20px;font-weight:bold;",
+          .cssSelector = ".box-header h3"
+        ) %>%
+        to_col_xl(),
       column(
         9,
         div(
@@ -89,6 +99,9 @@ page_servicesUI <- function(...){
         )
       )
     )
+
+
+  )
 
 
 
