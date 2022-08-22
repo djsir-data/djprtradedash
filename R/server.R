46175232c30a1820c69fc9141d3cdff258ca48ac
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-08-22 15:38:56 +1000

    fixed sitc levels in merch explorer

--- R/server.R
+++ R/server.R
@@ -48,5 +48,56 @@ server <- function(input, output, session) {
       )
     })
 
+  # Merch explorer SITC options updating
+  # Chooses parent SITC or first child SITC
+  observeEvent(
+    input$merch_explorer_sitc,
+    {
+      # Target SITC level
+      new_level <- req(input$merch_explorer_sitc)
+
+
+      # Data frame of current inputs
+      current_vals <- merch_sitc_lu[
+        match(input$merch_sitc, merch_sitc_lu$sitc),
+        ]
+
+      # All SITC and no selections
+      if(new_level == "All" & nrow(current_vals) == 0){
+        new_options <- merch_sitc_lu
+        new_selection <- character(0)
+      # ALL SITC with selections
+      } else if (new_level == "All"){
+        new_options <- merch_sitc_lu
+        new_selection <- sort(current_vals$sitc)
+      # Specific SITC with no selections
+      } else if (nrow(current_vals) == 0){
+        new_level <- as.integer(new_level)
+        new_options <- merch_sitc_lu[merch_sitc_lu$n == new_level, ]
+        new_selection <- character(0)
+      # Specific SITC with selections
+      } else {
+        new_level <- as.integer(new_level)
+        new_options <- merch_sitc_lu[merch_sitc_lu$n == new_level, ]
+        new_selection <- ifelse(
+          current_vals$n > new_level,
+          sapply(current_vals$sitc_code, function(x) pull(new_options[new_options$sitc_code == substr(x, 1, new_level), "sitc_code"])[1]),
+          sapply(current_vals$sitc_code, function(x) pull(new_options[substr(new_options$sitc_code, 1, nchar(x)) == x, "sitc_code"])[1])
+          )
+        new_selection <- pull(new_options[new_options$sitc_code %in% new_selection, "sitc"])
+      }
+
+      shinyWidgets::updateMultiInput(
+        session = session,
+        inputId = "merch_sitc",
+        selected = new_selection,
+        choices = sort(new_options$sitc)
+      )
+
+    }
+  )
+
+
+
 
 }
