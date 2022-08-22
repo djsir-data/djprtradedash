05e8b854123e896991661645b17a5d9079528277
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-08-23 09:39:50 +1000

    rm supplimentary data from db update

--- data-raw/create_dash_data.R
+++ data-raw/create_dash_data.R
@@ -14,23 +14,6 @@ merch_imp <- read_merch(series = "import")
 
 
 
-# Ensure merch files are data tables and filter for Victoria (to be reoved)
-data.table::setDT(merch)
-data.table::setDT(merch_imp)
-
-merch <- merch[origin == "Victoria"]
-merch_imp <- merch_imp[dest == "Victoria"]
-
-
-
-
-# # ABS International Trade Supplementary Information data
-# supp_cy <- read_supp("cy")
-# supp_fy <- read_supp("fy")
-
-
-
-
 # Service data
 services <- read_services()
 
@@ -47,8 +30,6 @@ bop <- read_bop()
 out <- list(
   'merch'         = merch,
   'merch_imp'     = merch_imp,
-  # 'supp_cy'       = supp_cy,
-  # 'supp_fy'       = supp_fy,
   'bop'           = bop,
   'service_trade' = services
   )
@@ -62,7 +43,7 @@ if(!exists("con")) load_tabs()
 
 
 
-# Add csv data to database
+# Add data to database
 mapply(
   FUN       = pool::dbWriteTable,
   conn      = list(con),
