e524e69c08046a3a4a83d858c59e79189d084cae
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-09-12 15:09:06 +1000

    Move db connection to server with cached globals & loader icons

--- app.R
+++ app.R
@@ -9,8 +9,20 @@ if(file.exists(".renviron")) readRenviron(".renviron")
 # Optional cached DB mode (use cacheDB() to create)
 # Sys.setenv(R_CONFIG_ACTIVE = "local_cache")
 
-# Establish database connections
-load_tabs()
+# Define global variables available to UI and server
+globals <- c(
+  "merch_dates",
+  "bop_dates",
+  "merch_sitc_lu",
+  "merch_country_dest",
+  "service_dates",
+  "service_categories"
+)
+
+lapply(globals, function(x){
+  assign(x, readRDS(paste0("inst/", x, ".rds")), envir = .GlobalEnv)
+})
+
 
 # Start shinyapp
 shiny::shinyApp(ui = ui(), server = server)
