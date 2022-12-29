f2562f7f856b42976f68414858aa7c92f246430c
Matt Johnson <mrjoh3@gmail.com>
2022-12-29 15:42:48 +1100

    change department name

--- R/page_disclaimer.R
+++ R/page_disclaimer.R
@@ -13,7 +13,7 @@ page_disclaimerUI <- function(...){
           " for information purposes only. No claim is made as to the accuracy",
           "or currency of any of the content on this website at any time. The ",
           "Victorian Government and this agency (the Department of Jobs, ",
-          "Precincts and Regions) do not accept any liability to any person",
+          "Skills, Industry and Regions) do not accept any liability to any person",
           "for the information (or the use of such information) which is",
           "provided on this website, including information sourced or derived",
           "from third parties or incorporated into the website by reference."
