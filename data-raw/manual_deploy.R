6bfd700036fb2849d3e5ab8bea4b94f68ff66ea4
Matt Johnson <mrjoh3@gmail.com>
2023-09-25 12:58:13 +1000

    remove pakrat opt

--- data-raw/manual_deploy.R
+++ data-raw/manual_deploy.R
@@ -1,4 +1,4 @@
-options(rsconnect.packrat = TRUE)
+#options(rsconnect.packrat = TRUE)
 
 # Write DB connection environment variables
 if(
@@ -20,7 +20,8 @@ if(
 # Publish App
 rsconnect::deployApp(
   appName = "djprtradedash-renv",
-  forceUpdate = TRUE
+  forceUpdate = TRUE,
+  envManagementR = NULL
 )
 
 
